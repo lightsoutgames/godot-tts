4893bf7e10c26224fd044ac9df602453fcec5fad
Nolan Darilek <nolan@thewordnerd.info>
2019-09-27 13:07:35 -0500

    Add getter/setter for rate.

--- TTS.gd
+++ TTS.gd
@@ -4,6 +4,13 @@ const TTS = preload("godot-tts.gdns")
 
 var tts = TTS.new()
 
+func set_rate(rate):
+    tts.rate = rate
+
+func get_rate():
+    return tts.rate
+
+var rate setget set_rate, get_rate
 func speak(text, interrupt := true):
     tts.speak(text, interrupt)
 
